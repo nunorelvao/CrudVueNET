a1dbe8d90412fb3bac86115a540f35af1417b1ad
Nuno Relvao <nunorelvao@github.com>
2020-04-26 18:23:09 +0100

    add new enviroment vars and buils for local

--- Controllers/SpaController.cs
+++ Controllers/SpaController.cs
@@ -10,9 +10,11 @@ namespace CrudVueNET.Controllers
     {
         // Enumerate all SPA urls so we boot the Vue app correctly
         // Alternatively, use this as the 404 page so any unhandled url will boot the vue app
-        [HttpGet("/")]
-        [HttpGet("/home")]
-        [HttpGet("/implicit/callback")]
-        public ActionResult Index(string id) => File("/index.html", "text/html");
+        //Any url scheme as /xxxx will be returned (all remaining slash will just retun not ound)
+        //[HttpGet("/{page}")]
+        //[HttpGet("/implicit/callback")]
+        [Route("/")] //route base without any slash
+        [Route("{*url:regex(^(?!api/).*$)}")] //Route all except the "api/"
+        public ActionResult Index(string url) => File("/index.html", "text/html");
     }
 }
